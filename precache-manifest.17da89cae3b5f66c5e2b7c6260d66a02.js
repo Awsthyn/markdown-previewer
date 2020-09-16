46292b0e7e6df63c6421bc3a3a4878fca3e7deb9
Awsthyn <aguswagner008gmail.com>
2020-09-15 21:44:16 -0300

    Updates

---
+++ precache-manifest.17da89cae3b5f66c5e2b7c6260d66a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efb8a2dc7b48e46435d8c79a1c12936",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "a7b9c749ea9c6315dc50",
+    "url": "/markdown-previewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9f0065e8fb0621f982b1",
+    "url": "/markdown-previewer/static/js/2.7bd850eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.7bd850eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b9c749ea9c6315dc50",
+    "url": "/markdown-previewer/static/js/main.68eabcab.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
